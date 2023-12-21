5364abdec360205d1002326308988b9b58c40c74
Nishchitha L R <nishunishugowda0@gmail.com>
2023-12-21 15:12:59 +0530

    Add project files.

---
+++ 21dec/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21dec
+{
+    internal class Program
+    {
+        public class Employee
+        {
+            public Employee() { Console.WriteLine(" am a C"); }
+            public void Display() { Console.WriteLine(" am display ()"); }
+            static void Main(string[] args)
+            {
+                Employee e1;
+                e1 = new Employee();// initializes class object , memory or instance,implicit
+                e1.Display();//explicit
+            }
+        }
+
+
+
+
+    }
+}
